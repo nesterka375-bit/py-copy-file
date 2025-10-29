df2fae313aba83fb51aedc1baa169ef7418ba1c1
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-10-29 08:11:02 +0100

    solved py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split(" ")
+    if len(parts) == 3 and parts[0] == "cp" and parts[1] != parts[2]:
+        cp, old_file, new_file = parts
+        try:
+            with (open(old_file, "r") as file_in,
+                  open(new_file, "w") as file_out):
+                file_out.write(file_in.read())
+        except Exception:
+            pass
