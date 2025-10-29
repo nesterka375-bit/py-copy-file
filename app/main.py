82a73eff34c4401dffc93c164341fe10cf7c8e63
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-10-29 08:16:04 +0100

    solved py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     parts = command.split(" ")
     if len(parts) == 3 and parts[0] == "cp" and parts[1] != parts[2]:
-        cp, old_file, new_file = parts
+        cp, source_file_name, destination_file_name = parts
         try:
-            with (open(old_file, "r") as file_in,
-                  open(new_file, "w") as file_out):
+            with (open(source_file_name, "r") as file_in,
+                  open(destination_file_name, "w") as file_out):
                 file_out.write(file_in.read())
-        except Exception:
+        except FileNotFoundError:
             pass
